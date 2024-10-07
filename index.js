dca3bc849f2f019c270d706c7909622419e10dd6
njiji <noelathandi123@gmail.com>
2024-10-07 14:02:49 +0300

    working with objects

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+
+const employee = {
+    name: "Same",
+    streetAdress: "11 Broadway",
+    
+}
+
+// 1. Non-destructive update
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, [key]: value };
+}
+
+// 2. Destructive update
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+// 3. Non-destructive delete
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// 4. Destructive delete
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
